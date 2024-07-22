b027e871feae2aaf4db9573ea26326a23e24b6b5
Gonzalo Zelaya <gonzalo.zelaya@outsourcearg.com>
2024-07-22 14:56:12 -0300

    Cambiado nombre del metodo payment method id

--- account-payment-group/models/models.py
+++ account-payment-group/models/models.py
@@ -3,8 +3,12 @@
 from odoo import models, fields, api
 
 class Account_payment_methods(models.Model):
-    _name = 'account.payment_methods'
-    
     _inherit = 'account.payment'
     
-    testing = fields.Char()
+    to_pay_move_line_ids = fields.Many2many(
+        comodel_name='account.move.line',
+        relation='account_payment_methods_move_line_rel',
+        column1='payment_method_id',
+        column2='move_line_id',
+        string='Move Lines to Pay'
+    )
