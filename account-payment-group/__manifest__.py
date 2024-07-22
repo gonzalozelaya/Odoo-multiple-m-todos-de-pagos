8e85802cbeb121b023cc4f171e6e9a9a06fb80fd
Gonzalo Zelaya <gonzalo.zelaya@outsourcearg.com>
2024-07-22 13:42:17 -0300

    Created module

---
+++ account-payment-group/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Account Payment with multiple methods",
+
+    'summary': """
+        Este modulo permite usar multiples metodos de pago en una sola orden""",
+
+    'description': """
+        Este modulo permite usar multiples metodos de pago en una sola orden
+    """,
+
+    'author': "OutsourceArg SAS",
+    'website': "http://www.yourcompany.com",
+    "license": "AGPL-3",
+    'installable': True,
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Payment',
+    'version': '17.0.0.1',
+
+    # any module necessary for this one to work correctly
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "account",
+        # TODO mover esto a modulo puente
+        "account_payment_pro",
+    ],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
