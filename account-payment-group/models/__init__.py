9c4a0cce885923c8bb15e39fcef74d13a956c6f9
Gonzalo Zelaya <gonzalo.zelaya@outsourcearg.com>
2025-02-20 14:26:10 -0300

    Updated module

--- account-payment-group/models/__init__.py
+++ account-payment-group/models/__init__.py
@@ -3,3 +3,4 @@
 from . import account_payment_multiple_methods
 from . import account_payment_register
 from . import account_journal
+from . import account_move
---
+++ account-payment-group/models/account_move.py
@@ -0,0 +1,35 @@
+import re
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+class AccountJournal(models.Model):
+    _inherit = 'account.move'
+    
+    open_move_line_ids = fields.One2many(
+        'account.move.line',
+        compute='_compute_open_move_lines'
+    )
+    pay_now_journal_id = fields.Many2one(
+        'account.journal',
+        'Pay now Journal',
+        help='If you set a journal here, after invoice validation, the invoice'
+        ' will be automatically paid with this journal. As manual payment'
+        'method is used, only journals with manual method are shown.',
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+        # use copy false for two reasons:
+        # 1. when making refund it's safer to make pay now empty (specially if automatic refund validation is enable)
+        # 2. on duplicating an invoice it's safer also
+        copy=False,
+    )
+    payment_group_ids = fields.Many2many(
+        'account.payment.group',
+        compute='_compute_payment_groups',
+        string='Payment Groups',
+        compute_sudo=True,
+    )
+
+    payment_group_id = fields.Many2one(
+        related='payment_id.payment_group_id',
+        store=True,
+    )
